38514b833ad8c45ff32732059a145d516547430d
Gio325 <175448374+Gio325@users.noreply.github.com>
2025-03-17 16:57:59 +0100

    first working version,added the playGame function and modified how the playRound function works

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
 // write a function that returns a rock paper or scissor
 
-function getComputerChoice(choice){
-    
-    choice = Math.floor(Math.random()* 3) +1;
-    if(choice === 1){
-        choice= "rock"
-    }else if(choice === 2){
-        choice= "paper"
-    }else{
+function getComputerChoice() {
+
+    choice = Math.floor(Math.random() * 3) + 1;
+    if (choice === 1) {
+        choice = "rock"
+    } else if (choice === 2) {
+        choice = "paper"
+    } else {
         choice = "scissors"
     }
-    
+
     return String(choice)
 }
+
 // write a function that takes the human choice and returns it
 
 // first i have to ask the user to input rock paper or scissors
-let usrchoice = prompt("rock paper or scissors?")
-function getHumanChoice(){
-    return usrchoice
+
+function getHumanChoice() {
+    let choice = prompt("rock paper or scissors?")
+    return  choice.toLowerCase();
 }
 
 
-// create a tracker of the player points using variables
-let humanScore = 0;
-let computerScore = 0;
+//  the logic to play an entire game
 
-// write the logic to play a single round
+function playGame() {
+    // create a tracker of the player points using variables
+    let humanScore = 0;
+    let computerScore = 0;
+    let rounds = 0;
 
-function playRound(HumanChoice, computerChoice){
-HumanChoice.toLowerCase();
-switch(HumanChoice, computerChoice){
-    case((getHumanChoice === "rock" && getComputerChoice === "scissors") || (getHumanChoice() === "paper" && getComputerChoice() === "rock") || (getHumanChoice() === "scissors" && getComputerChoice() === "paper")):
-    return "human wins the round!"
-    break
-    
-    case((getComputerChoice() === "rock" && getHumanChoice === "scissors") || (getComputerChoice() === "paper" && getHumanChoice() === "rock") || (getComputerChoice === "scissors" && getHumanChoice() === "paper")):
-    return "Computer wins the round!"
-    break
+    //  the logic to play a single round
+
+    function playRound(computerChoice, humanChoice) {
+        if ((computerChoice === "rock" && humanChoice === "scissors") ||
+            (computerChoice === "paper" && humanChoice === "rock") ||
+            (computerChoice === "scissors" && humanChoice === "paper")) {
+
+            return "computer chose: " + computerChoice + "." + "\nhuman chose: " + humanChoice + "." + "\ncomputer wins!" + "\ncomputer: " + ++computerScore + "\nhuman: " + humanScore
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+
+            return "human chose: " + humanChoice + "." + "\ncomputer chose: " + computerChoice + "." + "\nhuman wins the round!" + "\ncomputer: " + computerScore + "\nhuman: " + ++humanScore
+        } else {
+            return "computer chose: " + computerChoice + "." + "\nhuman chose: " + humanChoice + "." + "\nit's a tie!" + "\ncomputer: " + computerScore + "\nhuman: " + humanScore
+        }
+    }
+    while( rounds < 5){
+        console.log(playRound(getComputerChoice(), getHumanChoice()))
+        rounds++
+    }
+    if(computerScore === 5 && humanScore < 5){
+        return "computer won"
+    }else if(humanScore === 5 && computerScore < 5){
+        return "human won"
+    }else{
+        "remactch?"
+    }
 }
+console.log(playGame())
 
-}
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection)
-console.log(playRound(humanSelection, computerSelection));
-
-// write the logic to play the entire game
+// passo 1 prendere il numero del computer 
+// passo 2 prendere il numero del umano
+// passo 3 assegnarli alle variabili computerselection e humanselection
+// passo 4 ripetere l'assegnazione
+// 
